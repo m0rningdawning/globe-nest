e7dcb2eaf9bd0c2f27371d846d825aca3ee6ad58
Pavlo Korolov <bibonga54@gmail.com>
2024-01-17 01:05:03 +0100

    Discover screen improvement

---
+++ src/components/search/SearchBar.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/Ionicons";
+
+const SearchBar = () => {
+  const navigation = useNavigation();
+
+  const handlePress = () => {
+    navigation.navigate("Search" as never);
+  };
+
+  return (
+    <TouchableOpacity onPress={handlePress}>
+      <View style={styles.container}>
+        <Icon name="search-outline" style={styles.icon} />
+        <Text style={styles.input}>Search...</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    borderRadius: 10,
+    backgroundColor: "#323241",
+    marginTop: 20,
+  },
+  input: {
+    height: 40,
+    color: "#e0a16d",
+    paddingLeft: 10,
+    lineHeight: 40,
+  },
+  icon: {
+    fontSize: 20,
+    color: "#e0a16d",
+    lineHeight: 40,
+    paddingLeft: 10,
+  },
+});
+
+export default SearchBar;
--- src/screens/Discover.tsx
+++ src/screens/Discover.tsx
@@ -1,11 +1,17 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import SearchBar from "../components/search/SearchBar";
 
 const DiscoverScreen = () => {
   return (
-    <View style={[styles.container]}>
-      <Text>Discover Screen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headerWrapper}>
+        <Text style={styles.header}>Discover</Text>
+        <Text style={styles.subheader}>Those might interest you</Text>
+        <SearchBar />
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -13,8 +19,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#22222b",
-    justifyContent: "center",
-    alignItems: "center",
+  },
+  headerWrapper: {
+    flex: 1,
+    marginHorizontal: 20,
+    marginTop: 10,
+  },
+  header: {
+    fontSize: 28,
+    fontFamily: "Quicksand-Bold",
+    color: "#e0a16d",
+    textTransform: "uppercase",
+  },
+  subheader: {
+    fontSize: 16,
+    fontFamily: "Quicksand-Medium",
+    color: "#e0a16d",
+    textTransform: "uppercase",
   },
 });
 
