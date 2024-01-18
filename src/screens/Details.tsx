e070cd0fea2b50ac605a3977ce71bb8987415ef8
Pavlo Korolov <bibonga54@gmail.com>
2024-01-18 02:33:19 +0100

    Details screen added

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,12 +1,98 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Touchable,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
 import React from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/Ionicons";
+import { SafeAreaView } from "react-native-safe-area-context";
+import WebView from "react-native-webview";
 
 const DetailsScreen = () => {
+  const navigation = useNavigation();
+
+  const [isBookmarked, setIsBookmarked] = React.useState(false);
+  const [visible, setVisible] = React.useState(false);
+
+  const {params: item} = useRoute();
+
+  const toggleBookmark = async () => {};
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Details Screen</Text>
-    </View>
+    <>
+      <View style={styles.container}>
+        <SafeAreaView style={styles.header}>
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            style={styles.iconWrapper}
+          >
+            <Icon name="arrow-back-outline" style={styles.icon} />
+          </TouchableOpacity>
+          <View style={{ flexDirection: "row" }}>
+            <TouchableOpacity
+              onPress={() => navigation.goBack()}
+              style={styles.iconWrapper}
+            >
+              <Icon name="share-outline" style={styles.icon} />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => navigation.goBack()}
+              style={styles.iconWrapper}
+            >
+              <Icon
+                name="bookmark-outline"
+                style={styles.icon}
+                onPress={toggleBookmark}
+              />
+            </TouchableOpacity>
+          </View>
+        </SafeAreaView>
+        <WebView
+          source={{
+            // @ts-ignore
+            uri: item.url,
+          }}
+          onLoadStart={() => setVisible(true)}
+          onLoadEnd={() => setVisible(false)}
+          style={{ marginTop: 20 }}
+        />
+        {
+          visible && <ActivityIndicator size="large" color="#e0a16d" />
+        }
+      </View>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#22222b",
+  },
+  header: {
+    height: 60,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+    marginTop: 10,
+  },
+  iconWrapper: {
+    width: 35,
+    height: 35,
+    backgroundColor: "#e0a16d",
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 10,
+    marginHorizontal: 5,
+  },
+  icon: {
+    fontSize: 20,
+    color: "#22222b",
+  },
+});
 
 export default DetailsScreen;
--- src/screens/Saved.tsx
+++ src/screens/Saved.tsx
@@ -1,11 +1,153 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  Image,
+  Dimensions,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FlatList } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Icon from "react-native-vector-icons/Ionicons";
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
+import RecommendedNews from "../components/news/recommended/RecommendedNews";
+
+const { width } = Dimensions.get("window");
 
 const SavedScreen = () => {
+  const [saved, setSaved] = React.useState([]);
+  const [utlList, setUrlList] = useState([]);
+  const [bookmarkStat, setBookmarkStat] = useState([]);
+
+  // const onPress = (item) => {
+  //   // @ts-expect-error ts(2345)
+  //   navigation.navigate("Details", item);
+  // };
+
+  // const toggleBookmark = async (item, id) => {
+  //   try {
+  //     const savedBookmars = await AsyncStorage.getItem("savedBookmarks");
+  //     let bookmarks = savedBookmars ? JSON.parse(savedBookmars) : [];
+
+  //     const isBookmarked = bookmarks.some(
+  //       (savedBookmark) => savedBookmark.url === item.url
+  //     );
+
+  //     let newBookmarkStat = { ...bookmarkStat };
+
+  //     if (!isBookmarked) {
+  //       bookmarks.push(item);
+  //       await AsyncStorage.setItem("savedBookmarks", JSON.stringify(bookmarks));
+  //       newBookmarkStat[item.url] = true;
+  //     } else {
+  //       const updatedBookmarks = bookmarks.filter(
+  //         (bookmark) => bookmark.url !== item.url
+  //       );
+  //       await AsyncStorage.setItem(
+  //         "savedBookmarks",
+  //         JSON.stringify(updatedBookmarks)
+  //       );
+  //       newBookmarkStat[item.url] = false;
+  //     }
+
+  //     setBookmarkStat(newBookmarkStat);
+  //   } catch (error) {
+  //     console.log("Bookmarking error: " + error);
+  //   }
+  // };
+
+  // const renderItem: React.FC<{
+  //   item: any;
+  //   id: any;
+  // }> = ({ item, id }) => {
+  //   const date = new Date(item.publishedAt);
+
+  //   const formattedDate = date.toLocaleDateString("en-US", {
+  //     weekday: "short",
+  //     hour: "numeric",
+  //     day: "numeric",
+  //     month: "short",
+  //     year: "numeric",
+  //   });
+
+  //   return (
+  //     <TouchableOpacity
+  //       style={itemStyles.item}
+  //       key={id}
+  //       onPress={() => onPress(item)}
+  //     >
+  //       <View style={itemStyles.imageWrapper}>
+  //         <Image source={{ uri: item.urlToImage }} style={itemStyles.image} />
+  //       </View>
+  //       <View style={itemStyles.content}>
+  //         <Text style={itemStyles.title}>
+  //           {item?.title?.length > 60
+  //             ? item?.title?.slice(0, 60) + "..."
+  //             : item?.title}
+  //         </Text>
+  //         <Text style={itemStyles.source}>
+  //           {item.author ? item.author + ", " : ""}
+  //           {item.source.name ? item.source.name : ""}
+  //         </Text>
+  //         <Text style={itemStyles.date}>{formattedDate}</Text>
+  //       </View>
+  //       <TouchableOpacity
+  //         style={itemStyles.bookmarkContainer}
+  //         onPress={() => toggleBookmark(item, id)}
+  //       >
+  //         <Icon
+  //           name={bookmarkStat[item.url] ? "bookmark" : "bookmark-outline"}
+  //           style={itemStyles.bookmark}
+  //         />
+  //       </TouchableOpacity>
+  //     </TouchableOpacity>
+  //   );
+  // };
+
+  // useEffect(() => {
+  //   const urls = saved.map((item) => item.url);
+  //   setUrlList(urls);
+  // }, [saved]);
+
+  // useEffect(() => {
+  //   const loadBookmarks = async () => {
+  //     try {
+  //       const savedBookmarks = await AsyncStorage.getItem("savedBookmarks");
+  //       let bookmarks = savedBookmarks ? JSON.parse(savedBookmarks) : [];
+
+  //       let newBookmarkStat = {};
+  //       bookmarks.forEach((bookmark) => {
+  //         newBookmarkStat[bookmark.url] = true;
+  //       });
+  //       //@ts-expect-error ts(2345)
+  //       setBookmarkStat(newBookmarkStat);
+  //     } catch (error) {
+  //       console.log("Loading bookmarks error: " + error);
+  //     }
+  //   };
+
+  //   loadBookmarks();
+  // }, []);
+
   return (
-    <View style={[styles.container]}>
-      <Text>Saved Screen</Text>
-    </View>
+    <SafeAreaView style={[styles.container]}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        stickyHeaderIndices={[0]}
+        scrollEventThrottle={16}
+      >
+        <View style={styles.header}>
+          <Text style={styles.title}>Saved</Text>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Clear</Text>
+          </TouchableOpacity>
+        </View>
+        {/* <RecommendedNews label={"Saved"} data={saved} /> */}
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
@@ -13,8 +155,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#22222b",
-    justifyContent: "center",
-    alignItems: "center",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  title: {
+    fontSize: 30,
+    fontFamily: "Quicksand-Bold",
+    textTransform: "uppercase",
+    color: "#e0a16d",
+  },
+  button: {
+    backgroundColor: "#e0a16d",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontSize: 15,
+    fontFamily: "Quicksand-Medium",
+    color: "#22222b",
   },
 });
 
